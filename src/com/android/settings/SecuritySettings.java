e22fa2532cc1149c329e0bbcf2da40d08a55376d
Eugene San (eugenesan) <eugenesan@gmail.com>
2012-09-30 14:10:51 +0200

    optional encryption password sync
    Option to use 2 different tokens for encryption and locking.
    Allows convenient usage and strong encryption at the same time.
    
    Inspired by reading next:
    https://santoku-linux.com/howto/mobile-forensics/how-to-brute-force-android-encryption
    
    Related discussion:
    http://code.google.com/p/android/issues/detail?id=29468
    
    Change-Id: I71a31e7a04b84910d5be317e6f531c41d7ad002f

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -81,6 +81,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
     private static final String KEY_LOCK_BEFORE_UNLOCK = "lock_before_unlock";
     private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
+    private static final String LOCK_SYNC_ENCRYPTION_PASSWORD = "lock_sync_encryption_password";
     public static final String KEY_VIBRATE_PREF = "lockscreen_vibrate";
 
     DevicePolicyManager mDPM;
@@ -616,6 +617,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
             lockPatternUtils.setTactileFeedbackEnabled(isToggled(preference));
         } else if (KEY_LOCK_BEFORE_UNLOCK.equals(key)) {
             lockPatternUtils.setLockBeforeUnlock(isToggled(preference));
+        } else if (LOCK_SYNC_ENCRYPTION_PASSWORD.equals(key)) {
+            lockPatternUtils.setSyncEncryptionPassword(isToggled(preference));
         } else if (KEY_POWER_INSTANTLY_LOCKS.equals(key)) {
             lockPatternUtils.setPowerButtonInstantlyLocks(isToggled(preference));
         } else if (preference == mSlideLockDelayToggle) {
