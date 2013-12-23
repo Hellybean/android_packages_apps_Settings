3aec2dec8c55eee04e63bc7d2419077121453e06
Chester Kener <Cl3Kener@gmail.com>
2013-12-23 17:05:25 -0500

    Fix Dual Panel in HK Menu
    * We have been missing a few dependencies for quite some time.  Now it actually matters for Dual Panel.
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    One more
    
    Just to be safe

--- src/com/android/settings/hellkat/InterfaceSettings.java
+++ src/com/android/settings/hellkat/InterfaceSettings.java
@@ -3,17 +3,23 @@ package com.android.settings.hellkat;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class InterfaceSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
