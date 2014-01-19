9c1526d4f359ed17d759ef5dea1697a01645db27
martincz <martincz@mokeedev.com>
2014-01-20 00:16:19 +0800

    Settings: Force show navigation bar (2/2)
    Change-Id: I443a1816a2d2cf4fa3cf5302b544e54d19fdbca3

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -37,6 +37,9 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "SystemSettings";
 
+    // Force show navigation bar
+    private static final String KEY_FORCE_SHOW_NAVIGATION_BAR = "force_show_navigation_bar";
+	
     private static final String KEY_EXPANDED_DESKTOP = "expanded_desktop";
     private static final String KEY_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
     private static final String CATEGORY_NAVBAR = "navigation_bar";
@@ -45,6 +48,9 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
 
     // Enable/disable nav bar	
     private static final String ENABLE_NAVIGATION_BAR = "enable_nav_bar";
+
+    // Force show navigation bar
+    private CheckBoxPreference mForceShowNavigationBarPref;
 
     private ListPreference mExpandedDesktopPref;
     private CheckBoxPreference mExpandedDesktopNoNavbarPref;
@@ -59,6 +65,10 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
 
         addPreferencesFromResource(R.xml.system_ui_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mForceShowNavigationBarPref =
+                (CheckBoxPreference) findPreference(KEY_FORCE_SHOW_NAVIGATION_BAR);
+        mForceShowNavigationBarPref.setOnPreferenceChangeListener(this);
 
         // Expanded desktop
         mExpandedDesktopPref = (ListPreference) findPreference(KEY_EXPANDED_DESKTOP);
@@ -76,23 +86,26 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
 
         try {
             boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
-
+            boolean mHasNavigationBar = getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+            PreferenceCategory mCategoryNavbar = (PreferenceCategory) findPreference(CATEGORY_NAVBAR);
             if (hasNavBar) {
                 mExpandedDesktopPref.setOnPreferenceChangeListener(this);
                 mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
                 updateExpandedDesktop(expandedDesktopValue);
                 prefScreen.removePreference(mExpandedDesktopNoNavbarPref);
 
-                if (!Utils.isPhone(getActivity())) {
-                    PreferenceCategory navCategory =
-                            (PreferenceCategory) findPreference(CATEGORY_NAVBAR);
-                    navCategory.removePreference(mNavigationBarLeftPref);
+                if (mHasNavigationBar) {
+                    mCategoryNavbar.removePreference(mForceShowNavigationBarPref);
                 }
             } else {
                 // Hide no-op "Status bar visible" expanded desktop mode
                 mExpandedDesktopNoNavbarPref.setOnPreferenceChangeListener(this);
                 mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
                 prefScreen.removePreference(mExpandedDesktopPref);
+
+                // Hide navigation bar category
+                mCategoryNavbar.removeAll();
+                mCategoryNavbar.addPreference(mForceShowNavigationBarPref);
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error getting navigation bar status");
@@ -123,12 +136,16 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
             boolean value = (Boolean) objValue;
             updateExpandedDesktop(value ? 2 : 0);
             return true;
+
 	// Enable/disbale nav bar (used in custom nav bar dimensions)
 	} else if (preference == mEnableNavigationBar) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_SHOW,
                     ((Boolean) objValue) ? 1 : 0);
             updateNavbarPreferences((Boolean) objValue);
+        } else if (preference == mForceShowNavigationBarPref) {
+            Settings.System.putInt(resolver, Settings.System.FORCE_SHOW_NAVIGATION_BAR,
+                    (Boolean) objValue ? 1 : 0);
             return true;
         }
 
