b88628b26b58f3adab1cf7b291fefaacc2dc92e8
thdDude <afganidude@gmail.com>
2014-01-30 10:35:23 -0600

    Remove Help from Ram Bar

--- src/com/android/settings/cyanogenmod/RamBar.java
+++ src/com/android/settings/cyanogenmod/RamBar.java
@@ -52,8 +52,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
     private static final String RAM_BAR_COLOR_CACHE_MEM = "ram_bar_color_cache_mem";
     private static final String RAM_BAR_COLOR_TOTAL_MEM = "ram_bar_color_total_mem";
 
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int MENU_HELP = MENU_RESET + 1; 
+    private static final int MENU_RESET = Menu.FIRST; 
 
     private static final String EXPLANATION_URL = "http://www.slimroms.net/index.php/faq/slimbean/238-why-do-i-have-less-memory-free-on-my-device";
 
@@ -120,9 +119,6 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
         menu.add(0, MENU_RESET, 0, R.string.ram_bar_button_reset)
                 .setIcon(R.drawable.ic_settings_backup) // use the backup icon
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        menu.add(0, MENU_HELP, 0, R.string.ram_bar_button_help)
-                .setIcon(R.drawable.ic_settings_about)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS); 
     }
 
     @Override
@@ -130,11 +126,6 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
         switch (item.getItemId()) {
             case MENU_RESET:
                 resetToDefault();
-                return true;
-            case MENU_HELP: 
-                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(EXPLANATION_URL));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.startActivity(intent);
                 return true;
             default: 
                 return super.onContextItemSelected(item);
